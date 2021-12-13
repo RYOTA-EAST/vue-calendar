0ff3498f3243e5c8c78aedc6cd84b9e099e73ad0
Ryota Higashi <ryo19870130@outlook.jp>
2021-12-13 20:20:12 +0900

    front first commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <HelloWorld message="Hello World! from App.vue" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+};
+</script>
---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <p>Hello World! Hello World! Hello World! Hello World! Hello World! Hello World!</p>
+    <p>{{ message }}</p>
+    <!-- <input v-model="message"> -->
+    <ul>
+      <li v-for="item in items" :key="item.id">
+        {{ item.text }}
+      </li>
+    </ul>
+    <button @click="clickEvent()">click</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: ['message'],
+  data: () => ({
+    // message: 'hello world',
+    items: [
+      { id: 1, text: 'アイテム1' },
+      { id: 2, text: 'アイテム2' },
+      { id: 3, text: 'アイテム3' },
+    ],
+  }),
+  methods: {
+    clickEvent() {
+      console.log('clicked!');
+    },
+  },
+};
+</script>
