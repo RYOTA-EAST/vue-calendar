deec10f7d861a97333b8e6ad869879ef7d25adad
Ryota Higashi <ryo19870130@outlook.jp>
2021-12-30 16:04:24 +0900

    API作成

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -2,3 +2,5 @@ Event.create!(name: '予定1', start: '2021-12-12 10:00:00', end: '2021-12-12 11
 Event.create!(name: '予定2', start: '2021-12-13 10:00:00', end: '2021-12-13 11:00:00')
 Event.create!(name: '予定3', start: '2021-12-14 10:00:00', end: '2021-12-14 11:00:00')
 Event.create!(name: '予定4', start: '2021-12-15 10:00:00', end: '2021-12-15 11:00:00')
+
+Calendar.create!(name: '予定')
